87eea3a7879cebb0bf737976521013b28090cc0f
irshadkk <you@example.com>
2018-06-14 17:04:39 +0530

    Christo bug fixes

--- src/app/admin/survey/create-survey/survey-info/survey-info.component.ts
+++ src/app/admin/survey/create-survey/survey-info/survey-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input ,OnChanges,SimpleChanges } from '@angular/core';
 
 import { AdminService } from '../../../services/admin.service';
 
@@ -9,6 +9,7 @@ import { AdminService } from '../../../services/admin.service';
 })
 export class SurveyInfoComponent implements OnInit {
   @Input() surveyObj: any;
+  @Input() reset: boolean;
   
   loading: boolean = false;
   companies = [];
@@ -23,7 +24,20 @@ export class SurveyInfoComponent implements OnInit {
 
     this.loadCompanies();
   }
-
+  ngOnChanges(changes: SimpleChanges): void{
+   if(changes.reset.currentValue!=changes.reset.previousValue){    
+    this.surveyObj.surveyInfo.companyId = ""
+    this.surveyObj.surveyInfo.surveyId = ""
+    this.surveyObj.surveyInfo.name = ""
+    this.surveyObj.surveyInfo.description = ""
+    this.surveyObj.surveyInfo.welcomeMessage = ""
+    this.surveyObj.surveyInfo.exitMessage = ""
+    this.surveyObj.surveyInfo.startDate = null
+    this.surveyObj.surveyInfo.endDate = null
+    this.surveyObj.surveyInfo.publicationDate = null
+    this.surveyObj.surveyInfo.expirationDate = null
+   }
+  }
   loadCompanies(){
     this.loading = true;
 
@@ -34,7 +48,8 @@ export class SurveyInfoComponent implements OnInit {
 
         this.loading = false;
       }
-    )
+    ) 
+    this.companies=JSON.parse(`[{"companyId":"0A7CCADC-F6AA-49F6-87A0-476F43C74756","companyName":"Pfizer"},{"companyId":"0ef5693f-b8fb-46b5-b861-f13e340389ea","companyName":"Tesla"},{"companyId":"2c922424-aba5-4605-9ae2-dce10c0c5c36","companyName":"Bayer"},{"companyId":"bd6a8476-9ee6-409f-86cd-7f05be9d8cc5","companyName":"Ranbaxy"},{"companyId":"cab8ccf1-ec7c-4eed-b32f-c33b19e95a4a","companyName":"BMW"}]`);
   }
   
   validate(){
